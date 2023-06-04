1fc51fd3a2737ad1df1c994194171e7c7274fccf
Asav-2122 <asav.pandya@gmail.com>
2023-06-04 12:03:03 +0530

    contact form is updated with the formik

--- src/components/layout/Body/ContactMe.js
+++ src/components/layout/Body/ContactMe.js
@@ -1,23 +1,35 @@
 import React from "react";
 import "./ContactMe.css";
+import { useFormik } from "formik";
 const ContactMe = () => {
+  const {handleSubmit,handleChange,values,resetForm} = useFormik({
+          initialValues : {
+            name : "",
+            email : "",
+            msg : ""
+          },
+          onSubmit:(values)=>{
+            console.log(JSON.stringify(values))
+            resetForm();
+          }
+  })
   return (
     <div className="contact-me">
-      <form id="contact">
+      <form id="contact" onSubmit={handleSubmit}>
         <span>CONTACT</span>
         <div>
           <p>Name:</p>
-          <input type="text" />
+          <input type="text" name="name" value={values.name} required onChange={handleChange}  />
         </div>
         <div>
           <p>Email:</p>
-          <input type="email" />
+          <input type="email" name="email" value={values.email} required onChange={handleChange}  />
         </div>
         <div>
           <p className="msg">Message:</p>
-          <textarea rows={4} cols={8} />
+          <textarea rows={4} cols={8} name="msg" value={values.msg} onChange={handleChange} />
         </div>
-        <button onClick={(e) => e.preventDefault()}>Submit</button>
+        <button  type="submit">Submit</button>
       </form>
     </div>
   );
